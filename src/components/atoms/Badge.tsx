3bfd4910ef10d0d08f8de6bf32229b3fad36c34b
Mariraja Selvakumar <marirajaselvakumar2000@gmail.com>
2025-12-28 21:40:53 +0530

    Badge, Button & Skeleton components added

---
+++ src/components/atoms/Badge.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-semibold transition-colors",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-primary-foreground",
+        positive: "bg-green-500/10 text-green-500",
+        negative: "bg-red-500/10 text-red-500",
+        neutral: "bg-muted text-muted-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+export function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
